16a4888df8a774dfb99c33a1c20083132bb05805
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-03-03 13:58:38 -0500

    updated

--- M2/higher-order-fn/higher-order-fn_exercises_part-1.js
+++ M2/higher-order-fn/higher-order-fn_exercises_part-1.js
@@ -2,7 +2,7 @@
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
-
+//none are higher order. No nested functions. No functions as a parameter
 function truthy() {
   return true;
 }
@@ -17,13 +17,13 @@ falsy(); //> false
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
-
+//No because it's not invoking the previously defined functions or using them as a parameter
 function left() {
-  return 'Left';
+  return "Left";
 }
 
 function right() {
-  return 'Right';
+  return "Right";
 }
 
 function go(goLeft = true) {
@@ -34,7 +34,7 @@ function go(goLeft = true) {
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
-
+//Yes, this is a higher order function because it has functions as parameters on line 49.
 const directions = [];
 
 function storeDirections(direction) {
@@ -46,9 +46,9 @@ function go(direction, operation) {
   return direction;
 }
 
-go('Left', storeDirections);
-go('Right', storeDirections);
-go('Up', storeDirections);
+go("Left", storeDirections);
+go("Right", storeDirections);
+go("Up", storeDirections);
 console.log(directions); //> ["Left", "Right", "Up"]
 
 /**
@@ -57,13 +57,50 @@ console.log(directions); //> ["Left", "Right", "Up"]
  */
 
 function transform(text, fn) {
-  if (typeof fn !== 'function') {
+  if (typeof fn !== "function") {
     return text;
   }
 
   return fn(text);
 }
 
+function capitalize() {
+  return function(string) {
+    let capital = string.toUpperCase()
+    return capital;
+  };
+}
+console.log(transform('coding', capitalize()));
+
+function lowercase() {
+  return function(string) {
+    let lowercased = string.toLowerCase();
+    return lowercased;
+  }
+}
+console.log(transform('CodEr', lowercase()));
+
+function dashes() {
+  return function(string) {
+    let dashedWords = string.replaceAll(' ', '-');
+    return dashedWords;
+  }
+}
+console.log(transform('code code code', dashes()));
+
+function acronym() {
+  let str = '';
+  return function(string) {
+    let uppercase = string.toUpperCase()
+    let words = uppercase.split(' ');
+    for (let letter of words) {
+      let firstLetter = letter.charAt(0)
+      str += firstLetter
+    }
+    return str;
+  }
+}
+console.log(transform('up down left', acronym()))
 /**
  * Use the function above to transform the text in the following ways. For each line below, you will need to write an anonymous function.
  *
