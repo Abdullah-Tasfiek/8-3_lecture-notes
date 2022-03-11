5f5d86d22e7c0d3e14db3983e884c8c6c865f3c1
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-03-11 11:53:14 -0500

    updated

--- M2/higher-order-native-array-methods/reduce.js
+++ M2/higher-order-native-array-methods/reduce.js
@@ -39,7 +39,7 @@ function sum(arr) {
   return res;
 }
 
-console.log(sum(numbers));
+// console.log(sum(numbers));
 
 // accumulator pattern + higher order function
 
@@ -60,7 +60,7 @@ function getSum(a, b) {
   return a + b;
 }
 
-console.log(getArrSum(numbers, getSum));
+// console.log(getArrSum(numbers, getSum));
 
 // accumulator pattern + reduce native array method
 let sum0 = numbers.reduce((accumulator, currentValue) => {
@@ -69,7 +69,7 @@ let sum0 = numbers.reduce((accumulator, currentValue) => {
   return accumulator + currentValue; // in the background the accumulator value is updated to equal this result of this expression
 }, 0); // step 1 of accumulator pattern (default value and type)
 
-console.log(sum0);
+// console.log(sum0);
 
 let numbers2 = [1, 2, 3, 4];
 
@@ -78,7 +78,7 @@ let sum1 = numbers2.reduce((acc, el) => {
   return acc + el;
 });
 
-console.log(sum1); // => 10
+// console.log(sum1); // => 10
 
 // validate our element values are correct
 let numbers3 = ['string'];
@@ -90,7 +90,7 @@ let sumNumbers = numbers3.reduce((acc, el) => {
   return acc + el;
 }, 0);
 
-console.log(sumNumbers);
+// console.log(sumNumbers);
 
 // handling an empty array
 let numbers4 = [];
@@ -102,7 +102,7 @@ let sumNumbers4 = numbers4.reduce((acc, el) => {
   return acc + el;
 }, 0);
 
-console.log(sumNumbers4);
+// console.log(sumNumbers4);
 
 /**
  * 2
@@ -144,10 +144,17 @@ let sum3 = arr2.reduce(adder, 5);
  *
  * Turn an array of numbers into a long string of all those numbers.
  */
-
+const arr = [5, 6, 5, 4, 5]
 function stringConcat(arr) {
-  // your code here
+  let string = arr.reduce((accumulate, firstValue) => {
+    if (typeof accumulate === 'number') {
+      firstValue = firstValue.toString();
+    }
+    return accumulate + firstValue;
+  }); 
+  return string;
 }
+console.log(stringConcat(arr));
 
 // console.log(stringConcat([1, 2, 3])); // "123"
 /**
@@ -157,12 +164,7 @@ function stringConcat(arr) {
  *
  * Note: You don't necessarily have to use reduce for this, so try to think of multiple ways you could solve this.
  */
-
-function totalVotes(arr) {
-  // your code here
-}
-
-const voters = [
+ const voters = [
   { name: 'Bob', age: 30, voted: true },
   { name: 'Jake', age: 32, voted: true },
   { name: 'Kate', age: 25, voted: false },
@@ -176,7 +178,17 @@ const voters = [
   { name: 'Jeff', age: 30, voted: true },
   { name: 'Zack', age: 19, voted: false },
 ];
-// console.log(totalVotes(voters)); // 7
+
+function totalVotes(arr) {
+  let count = arr.reduce((acc, curr) => {
+    if (curr.voted === true){
+      acc++
+    }
+    return acc
+  },0)
+  return count
+}
+console.log(totalVotes(voters)); // 7
 
 /**
  * 6
@@ -185,9 +197,11 @@ const voters = [
  */
 
 function shoppingSpree(arr) {
-  // your code here
+  let count = arr.reduce((accumulate, firstValue) => {
+    return accumulate + firstValue.price;
+  }, 0);
+  return count;
 }
-
 const wishlist = [
   { title: 'Tesla Model S', price: 90000 },
   { title: '4 carat diamond ring', price: 45000 },
@@ -195,6 +209,8 @@ const wishlist = [
   { title: 'Gold fidgit spinner', price: 2000 },
   { title: 'A second Tesla Model S', price: 90000 },
 ];
+
+console.log(shoppingSpree(wishlist));
 
 // console.log(shoppingSpree(wishlist)); // 227005
 /**
@@ -206,12 +222,15 @@ const wishlist = [
  */
 
 function flatten(arr) {
-  // your code here
+  let count = arr.reduce((accumulate, firstValue) => {
+    return accumulate.concat(firstValue)
+  }, []);
+  return count
 }
 
 const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
 
-// console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
 
 /**
  * 8
@@ -220,9 +239,27 @@ const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
  *
  * Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
  */
-
+// const voters = [
+//   { name: 'Bob', age: 30, voted: true },
+//   { name: 'Jake', age: 32, voted: true },
+//   { name: 'Kate', age: 25, voted: false },
+//   { name: 'Sam', age: 20, voted: false },
+//   { name: 'Phil', age: 21, voted: true },
+//   { name: 'Ed', age: 55, voted: true },
+//   { name: 'Tami', age: 54, voted: true },
+//   { name: 'Mary', age: 31, voted: false },
+//   { name: 'Becky', age: 43, voted: false },
+//   { name: 'Joey', age: 41, voted: true },
+//   { name: 'Jeff', age: 30, voted: true },
+//   { name: 'Zack', age: 19, voted: false },
+// ];
+//how many of the age, and how many of them voted
 function voterResults(arr) {
   // your code here
+  let count = arr.reduce((accumulate, firstValue) => {
+    
+
+
 }
 
 // console.log(voterResults(voters)); // Returned value shown below:
