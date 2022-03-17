04d055b3073583cef9db219647cc323d0f66ad4c
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-03-17 16:06:51 -0400

    updated notes

--- M2/mid-module-assessment/todo-exercises.js
+++ M2/mid-module-assessment/todo-exercises.js
@@ -31,6 +31,12 @@ function isTodoDueOn() {}
  * allRelatedItemsComplete
  *
  * write a function with two parameters (todos and id), that checks if EVERY todo item related to the given id is complete.
+ * given an ID
+ * At the given ID (argument), check each elements related.To[], whichcontains an array of ids
+ * once you have the todos.relatedTo, check each element and find that ID in the entire array of objects (todos).
+ * each id in the array must be true, for the function to return true
+ * find out if that todos.status.complete === true || false
+ * if an id DNE, throw an error
  * If no todo item matches the id provided, your function should throw an error with a helpful error message.
  *
  * @param {Object[]} todos
@@ -41,7 +47,30 @@ function isTodoDueOn() {}
  *   => ex: allRelatedItemsComplete(exampleTodos, "ehw49g0") // => false
  *   => ex: allRelatedItemsComplete(exampleTodos, "zbacasq") // ERROR
  */
-function allRelatedItemsComplete() {}
+function allRelatedItemsComplete(todos, id) {
+  // find todo object that matches given id (.find( might be helpful...))
+  let todoItem = todos.find((todo) => {
+    todo.id === id;
+  });
+
+  // if we don't find a matching todo item... throw an error
+  if (!todoItem) {
+    throw `Could not find todo item with id: ${id}`;
+  }
+
+  //check if every related item is complete
+  return todoItem.relatedTo.every((relatedId) => {
+    let relatedToDoItem = todos.find((todo) => todo.id === relatedId);
+    return relatedToDoItem.status.complete;
+  });
+  //
+}
+// allRelatedItemsComplete(exampleTodos, "ba9tyk4"); // => true
+// allRelatedItemsComplete(exampleTodos, "ehw49g0"); // => false
+// allRelatedItemsComplete(exampleTodos, "zbacasq"); // ERROR
+//1. Understand the problem
+//  -> Inputs: todos (Object[]) and id (string)
+//  -> return value: boolean || Error
 
 /**
  * getStatuses
@@ -65,7 +94,16 @@ function getStatuses() {}
  *
  *   => ex: getIncompleteDescriptions(exampleTodos) // => ["Make dinner", "Clean bedroom", "Sweep floor"]
  */
-function getIncompleteDescriptions() {}
+function getIncompleteDescriptions() {
+    // filter todo list, leaving only the incomplete items
+    // create filter callback 
+    //takes a single todo object and checks if its incomplete
+    const isIncomplete = (todo) => !todo.status.complete
+    const getDescription = (todo) => todo.description;
+    // map each todo object to its description
+    return todos.filter(isIncomplete).map(getDescription);
+}
+console.log(getIncompleteDescriptions(exampleTodos));
 
 // Now it's your turn! Don't stop here:
 // come up with additional practice problems based on the todo list that will help you practice higher order array methods, error handling, and ES6 syntax
